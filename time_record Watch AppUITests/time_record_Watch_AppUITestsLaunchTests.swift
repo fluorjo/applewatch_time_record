8c288e6f76fae12b2b4cc6a66f75292b76c1126f
fluorjo <fluorjo@naver.com>
2023-07-10 15:24:18 +0900

    Initial Commit

---
+++ time_record Watch AppUITests/time_record_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  time_record_Watch_AppUITestsLaunchTests.swift
+//  time_record Watch AppUITests
+//
+//  Created by fluor on 2023/07/10.
+//
+
+import XCTest
+
+final class time_record_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
