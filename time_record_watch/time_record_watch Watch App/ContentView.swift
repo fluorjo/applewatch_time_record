6bcd47f1d99ad6e01783c04dbb0095c0434a5917
fluorjo <fluorjo@naver.com>
2023-07-12 20:51:30 +0900

    watch app 추가

---
+++ time_record_watch/time_record_watch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  time_record_watch Watch App
+//
+//  Created by fluor on 2023/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ time_record_watch/time_record_watch Watch App/time_record_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_record_watchApp.swift
+//  time_record_watch Watch App
+//
+//  Created by fluor on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct time_record_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
