d46e8a8f7ccb91596b76db45d9cf408182497f82
fluorjo <fluorjo@naver.com>
2023-08-20 16:50:54 +0900

    apptranslate_placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                AppLocalizations.of(context)!.appTitle("wwww"),
+                AppLocalizations.of(context)!.titleOfApp("wwww"),
               ),
               Text(
                 '$_counter',
