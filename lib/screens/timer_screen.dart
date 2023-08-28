b1c612b602d1a960c8401df714e137b5951c22bc
fluorjo <fluorjo@naver.com>
2023-08-28 18:05:12 +0900

    runtimer

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
@@ -59,6 +61,45 @@ class _TimerScreenState extends State<TimerScreen> {
       _timer = WORK_SECONDS;
       _timerStatus = TimerStatus.stopped;
       print("[=>] $_timerStatus");
+    });
+  }
+
+  void runTimer() async {
+    Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      switch (_timerStatus) {
+        case TimerStatus.paused:
+          t.cancel();
+          break;
+        case TimerStatus.stopped:
+          t.cancel();
+          break;
+        case TimerStatus.running:
+          if (_timer <= 0) {
+            showToast("작업 완료!");
+            rest();
+          } else {
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+        case TimerStatus.resting:
+          if (_timer <= 0) {
+            setState(() {
+              _pomodoroCount += 1;
+            });
+            showToast("오늘 $_pomodoroCount개의 뽀모도로를 달성했습니다.");
+            t.cancel();
+            stop();
+          } else {
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+        default:
+          break;
+      }
     });
   }
 
